62fb6acc36a5325599f607f768054d8c8ad51a77
Molodoy <mavlatova.aza@yandex.ru>
2023-01-30 18:21:14 +0300

    решила 4 задачу

--- assss.py
+++ assss.py
@@ -6,11 +6,20 @@ import os
 
 def change_spn(flag):
     global spn
-    print(spn)
     if flag:
         spn = [spn[0] * 2, spn[1] * 2]
     else:
         spn = [spn[0] / 2, spn[1] / 2]
+
+
+def change_map():
+    global type_map
+    if type_map == "map":
+        type_map = "sat"
+    elif type_map == "sat":
+        type_map = "skl"
+    elif type_map == "skl":
+        type_map = "map"
 
 
 def show_map():
@@ -35,19 +44,20 @@ pygame.init()
 show_map()
 screen = pygame.display.set_mode((600, 450))
 running = True
-now = 0
 while running:
     screen.blit(pic, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
-            if event.key == 281:
+            if event.key == 109:  # Если нажимаем на M, то меняется тип карты
+                change_map()
+                show_map()
+            elif event.key == 281:
                 change_spn(True)
                 show_map()
             elif event.key == 280:
                 change_spn(False)
                 show_map()
-
     pygame.display.flip()
 pygame.quit()
